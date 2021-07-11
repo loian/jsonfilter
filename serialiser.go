815e204b6aa01d453d2cef3589ce59a998a9d419
lorenzo <lorenzo.iannone@gmail.com>
2021-07-11 11:29:14 +0100

    keep experimenting

--- serialiser.go
+++ serialiser.go
@@ -6,30 +6,30 @@ import (
 )
 
 type T3 struct {
-	Visa string `json:"visa"`
+	Visa  string `json:"visa"`
 	Notes string `json:"note"`
 }
 
 type T2 struct {
 	Notice int `json:"notice"`
 	Salary int `json:"salary"`
-	Wp T3 `json:"wp"`
+	Wp     T3  `json:"wp"`
 }
 
 type T1 struct {
-	Name string `json:"name"`
-	Id string `json:"id"'`
-	Expectations T2 `json:"expectations"`
+	Name         string `json:"name"`
+	Id           string `json:"id"'`
+	Expectations T2     `json:"expectations"`
 }
 
-var t T1 = T1 {
+var t T1 = T1{
 	Name: "mikhail",
-	Id: "1123jj1hh123",
-	Expectations: T2 {
+	Id:   "1123jj1hh123",
+	Expectations: T2{
 		Notice: 3,
 		Salary: 60000,
-		Wp: T3 {
-			Visa: "tier2",
+		Wp: T3{
+			Visa:  "tier2",
 			Notes: "foobar",
 		},
 	},
@@ -37,7 +37,7 @@ var t T1 = T1 {
 
 func access(data map[string]interface{}, keys []string) map[string]interface{} {
 	tmp, ok := data[keys[0]].(map[string]interface{})
-	if (!ok) {
+	if !ok {
 		return nil
 	}
 	if len(keys) > 1 {
@@ -60,19 +60,19 @@ func main() {
 	if err != nil {
 		//return err
 	}
-	fmt.Println (string(j))
+	fmt.Println(string(j))
 
 	var mp map[string]interface{}
-	err = json.Unmarshal([]byte(j),&mp)
+	err = json.Unmarshal([]byte(j), &mp)
 	if err != nil {
 		//return err
 	}
 
 	//delete (access(mp, []string{"expectations","wp"}),"visa")
 
-	remove (mp, []string{"name"})
-	remove (mp, []string{"expectations","wp","visa"})
+	remove(mp, []string{"name"})
+	remove(mp, []string{"expectations", "wpw", "visa"})
 
 	j, err = json.Marshal(mp)
-	fmt.Println (string(j))
+	fmt.Println(string(j))
 }
