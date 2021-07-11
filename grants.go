595cc66ead3280d6e9acb13c5cc2796eea7e919c
lorenzo <lorenzo.iannone@gmail.com>
2021-07-11 19:45:13 +0100

    first working prototype

--- grants.go
+++ grants.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+type Grant struct {
+	Path   []string `json:"path"`
+	Roles  []string `json:"roles"`
+	Action string   `json:"action"`
+}
+
+type Grants struct {
+	Grants []Grant `json:"grants"`
+}
+
+func readConfig(path string) (*Grants, error) {
+	//TODO: some sanitisation of the path
+	file, err := ioutil.ReadFile(path)
+	if err != nil {
+
+	}
+
+	data := Grants{}
+
+	err = json.Unmarshal([]byte(file), &data)
+	if err != nil {
+		return nil, err
+	}
+
+	return &data, nil
+}
